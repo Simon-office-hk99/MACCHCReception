d586615de666028f16e3008d7762136233e3201f
Simon-office-hk99 <92381187+Simon-office-hk99@users.noreply.github.com>
2024-01-05 01:24:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,11 @@ import { githubToken } from 'https://receptionmacchc.tiiny.site/config.js';
 // Make a GET request to retrieve the file details
 fetch(apiUrl)
   .then(response => response.json()) 
-   if (response.ok) {
-        console.log('Read File successfully.');
         .then(data => {
         // The SHA of the file is available in the response data
     	apiUrl_sha = data.sha;
     	console.log('SHA of the file:', apiUrl_sha);
   	})
-  } else {
-	console.error('Failed to read file:', response.statusText);
-  }
   .catch(error => {
     console.error('Error fetching file details:', error);
   });
